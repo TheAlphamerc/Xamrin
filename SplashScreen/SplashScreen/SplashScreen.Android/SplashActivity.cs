ddbc8b80762aafe98b2c963fe15f8733f35613a4
Sonu Sharma <sonu.sharma045@gmail.com>
2018-03-06 17:41:09 -0800

    Splash Screen Project Added

---
+++ SplashScreen/SplashScreen/SplashScreen.Android/SplashActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SplashScreen.Droid;
+
+namespace LoginAuthentication.Droid
+{
+    //[Activity(Label = "SplashActivity")]
+    //public class SplashActivity : Activity
+    //{
+    //    protected override void OnCreate(Bundle savedInstanceState)
+    //    {
+    //        base.OnCreate(savedInstanceState);
+
+    //        // Create your application here
+    //    }
+    //}
+
+    [Activity(Label = "myApp", Icon = "@drawable/icon", Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            var startUp = new Task(() =>
+            {
+                var intent = new Intent(this, typeof(MainActivity));
+                StartActivity(intent);
+            });
+            startUp.ContinueWith(t => Finish());
+
+            startUp.Start();
+        }
+    }
+}
